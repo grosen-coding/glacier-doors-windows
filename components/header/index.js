1ca8d1de05a630e3de780606a0d85b5a334abe22
Terry Rosen <tcrosen@gmail.com>
2023-02-12 14:02:56 -0500

    add navbar example

--- components/header/index.js
+++ components/header/index.js
@@ -1,14 +1,10 @@
 import React from "react";
-import Hero from "./hero";
 import Navbar from "./Navbar";
-import ServicesBar from "./ServicesBar";
 
 const Header = () => {
   return (
     <header>
       <Navbar />
-      <ServicesBar />
-      <Hero />
     </header>
   );
 };
